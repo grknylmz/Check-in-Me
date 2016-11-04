614971ba03505ea6e9ed4f3754a2cc2e66f142d6
Gurkan Yilmaz <gurkanyilmaz@gurkans-macbook-pro.local>
2016-11-04 13:27:46 +0200

    Initial Commit

---
+++ Check-in Me/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Check-in Me
+//
+//  Created by Gurkan Yilmaz on 04/11/16.
+//  Copyright © 2016 Gurkan Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
