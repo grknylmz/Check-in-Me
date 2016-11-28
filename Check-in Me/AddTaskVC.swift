4965d8f5591af66d98a740f9735392773a21a0ee
Gurkan Yilmaz <gurkanyilmaz@Gurkans-MacBook-Pro.local>
2016-11-28 21:47:13 +0300

    Task details

--- Check-in Me/AddTaskVC.swift
+++ Check-in Me/AddTaskVC.swift
@@ -10,9 +10,17 @@ import UIKit
 import Eureka
 
 class AddTaskVC: FormViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Görevler"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named : "ic_menu"),
+                                                                style: UIBarButtonItemStyle.plain,
+                                                                target: self,
+                                                                action: #selector(self.pop))
+        
+        
+        
         
         form = Section("Kullanıcı Bilgileri")
             <<< TextRow(){ row in
@@ -25,11 +33,18 @@ class AddTaskVC: FormViewController {
             }
             +++ Section("Görev Bilgileri")
             <<< DateRow(){
-                $0.title = "Görev"
+                $0.title = "Tarih"
                 $0.value = NSDate(timeIntervalSinceReferenceDate: 0) as Date
-                
+            }
+            <<< ButtonRow() {
+                $0.title = "Kaydet"
+                }
+                .onCellSelection { [weak self] (cell, row) in
+                    self?.pop()
         }
+        
     }
+
     
     func pop(){
         dismiss(animated: true, completion: nil)
