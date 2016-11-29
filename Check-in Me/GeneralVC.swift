bdc592c05ff16be2caf6600ce74928c32e5dedec
yoncacoskun <yonca.coskun@hotmail.com.tr>
2016-11-29 15:38:05 +0200

    Setting VC updated storyboard

--- Check-in Me/GeneralVC.swift
+++ Check-in Me/GeneralVC.swift
@@ -1,39 +1,38 @@
 //
-//  SettingVC.swift
+//  GeneralVC.swift
 //  Check-in Me
 //
-//  Created by Yonca Coskun on 25.11.2016.
+//  Created by Yonca Coskun on 29.11.2016.
 //  Copyright © 2016 Gurkan Yilmaz. All rights reserved.
 //
-
 import UIKit
 import Foundation
 
-class SettingVC: UITableViewController{
-    
-   // var selectedIndex:Int = 0
-    
-    @IBOutlet var cellTableView: UITableView!
+class GeneralVC: UIViewController{
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        
         
         self.title = "Ayarlar"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named : "ic_menu"),
                                                                 style: UIBarButtonItemStyle.plain,
                                                                 target: self,
                                                                 action: #selector(self.menu))
-       // personName.text = self.person?.getUserName()
-       // self.view.addSubview(personName)
-      
+        // personName.text = self.person?.getUserName()
+        // self.view.addSubview(personName)
+        
     }
     func menu() {
         self.openSideMenu()
     }
-
-   
-
+    
+        
+    
+    
+    
     
 }
+
